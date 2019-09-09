0815d41970988cc61d4dfca85f8f25082f40bc26
betomaluje <beto.maluje@gmail.com>
2019-09-09 07:19:52 +0200

    ADD now we change the mass of the object when resizing

--- Kirby Clone/Kirby Clone/Assets/Scripts/Skills/BodyChangerSkill.cs
+++ Kirby Clone/Kirby Clone/Assets/Scripts/Skills/BodyChangerSkill.cs
@@ -9,10 +9,17 @@ public class BodyChangerSkill : Skill
     public float timeForPower;
 
     private GameObject targetObject;
+    private float originalMass;
 
     public override void initSkill(GameObject skillObject)
     {
         targetObject = skillObject;
+
+        Rigidbody2D rb = targetObject.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            originalMass = rb.mass;
+        }
     }
 
     public override void performSkill(int direction)
@@ -21,13 +28,24 @@ public class BodyChangerSkill : Skill
 
     public override IEnumerator performCorroutineSkill(int direction)
     {
+        ChangeMass(scaleFactor);
         targetObject.transform.DOScale(scaleFactor, 0.5f);
         yield return new WaitForSeconds(timeForPower);
+        ChangeMass(originalMass);
         targetObject.transform.DOScale(1, 0.5f);
     }    
 
     public override void setPosition(Vector3 position)
     {
         
+    }
+
+    private void ChangeMass(float mass)
+    {
+        Rigidbody2D rb = targetObject.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.mass = mass;
+        }
     }
 }
