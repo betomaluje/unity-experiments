9120a9c42a75032be7b95e30f9028a9da394f8e3
Beto Maluje <beto.maluje@gmail.com>
2019-09-25 18:21:30 +0200

    ADD fade in and fade out animations ADD totem

--- Kirby Clone/Kirby Clone/Assets/Scripts/Menu/SceneLoader.cs
+++ Kirby Clone/Kirby Clone/Assets/Scripts/Menu/SceneLoader.cs
@@ -3,11 +3,24 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    [SerializeField] private Animator animator;    
+
+    private int nextSceneIndex;
+
     public void LoadScene(int scene) {
-        SceneManager.LoadScene(scene);
+        nextSceneIndex = scene;
+        animator.SetTrigger("FadeOut");    
+    }
+
+    public void LoadNextScene() {
+        LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void LoadOptions() {
 
+    }
+
+    public void OnFadeComplete() {
+        SceneManager.LoadScene(nextSceneIndex);
     }
 }
