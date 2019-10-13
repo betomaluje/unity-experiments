4509a6da52596472f0ad0e20c096a93a06a20030
betomaluje <beto.maluje@gmail.com>
2019-10-13 13:20:31 +0200

    ADD Orc game

---
+++ Orco/Assets/Scripts/PlayerGrabbing.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGrabbing : MonoBehaviour
+{
+    private Animator anim;
+    
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            anim.SetBool("isAttacking", true);
+        } else
+        {
+            anim.SetBool("isAttacking", false);
+        }
+    }
+}
---
+++ Orco/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+    [SerializeField] private float speed = 5f;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    private Vector2 movement;
+    private Vector2 mousePos;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        anim.SetBool("isWalking", movement.x != 0 || movement.y != 0);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+    }
+
+}
