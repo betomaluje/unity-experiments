2f1dd651ed6c38b30b9ac3924939b53442f22cec
betomaluje <beto.maluje@gmail.com>
2019-10-16 21:26:34 +0200

    UPDATE rooms and bridges (rotation on Bridges need to improve)

--- Orco/Assets/Scripts/Level/RandomLevelGenerator.cs
+++ Orco/Assets/Scripts/Level/RandomLevelGenerator.cs
@@ -7,6 +7,7 @@ public class RandomLevelGenerator : MonoBehaviour
     [SerializeField] private int numberOfRooms = 10;
     [SerializeField] private int scale;
     [SerializeField] private GameObject[] roomsObjects;
+    [SerializeField] private GameObject bridge;
     
     private List<Vector2> directionsList;
     private List<Vector3> roomPositions;
@@ -23,32 +24,42 @@ public class RandomLevelGenerator : MonoBehaviour
     }
 
     private void GenerateMap() {
-        roomPositions = new List<Vector3>();
-
-        Vector2 newPosition = directionsList[Random.Range(0, directionsList.Count)] * scale;
+        roomPositions = new List<Vector3>();    
 
         // we instantiate first room
-        Instantiate(getRoom(), transform.position, Quaternion.identity);
-        transform.position += new Vector3(newPosition.x, newPosition.y, 0);
+        Instantiate(getRoom(), transform.position, Quaternion.identity);        
+        roomPositions.Add(transform.position);
 
         print("first " + transform.position);
 
         for (int i = 0; i < numberOfRooms - 1; i++)
         {
-            newPosition = directionsList[Random.Range(0, directionsList.Count)] * scale;
+            Vector2 direction = GetRandomDirection();
+            Vector2 temp2DPosition = direction * scale;            
+            Vector3 nextPosition = new Vector3(temp2DPosition.x, temp2DPosition.y, 0) + transform.position;
 
-            if (!roomPositions.Contains(newPosition)) {
-                // we instantiate first room
-                Instantiate(getRoom(), transform.position, Quaternion.identity);
-                transform.position += new Vector3(newPosition.x, newPosition.y, 0);
+            if (!roomPositions.Contains(nextPosition)) {
+                // we need to connect current room with next one                
+                Vector2 bridge2DPosition = direction * scale / 2;
+                Vector3 bridgePosition = new Vector3(bridge2DPosition.x, bridge2DPosition.y, 0) + transform.position;
+                Instantiate(bridge, bridgePosition, Quaternion.identity);
 
-                roomPositions.Add(transform.position);
+                // we instantiate a room
+                Instantiate(getRoom(), nextPosition, Quaternion.identity);                
 
-                print("room " + i + " -> " + transform.position);
+                // we update the gameobjects position
+                transform.position = nextPosition;
+
+                roomPositions.Add(nextPosition);               
             } else {
                 i--;
             }       
         }
+    }
+
+    private Vector2 GetRandomDirection()
+    {
+        return directionsList[Random.Range(0, directionsList.Count)];
     }
 
     private GameObject getRoom() {
