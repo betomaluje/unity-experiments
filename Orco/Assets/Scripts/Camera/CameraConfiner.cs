5fa5bb9148cf8f62ff9a0c9c048f3192e0ff89b9
betomaluje <beto.maluje@gmail.com>
2019-10-17 20:15:29 +0200

    ADD post processing ADD camera shake

---
+++ Orco/Assets/Scripts/Camera/CameraConfiner.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+public class CameraConfiner : MonoBehaviour
+{
+    public CinemachineConfiner confiner;
+
+    // Start is called before the first frame update
+    public void ActivateConfiner() {
+        confiner.enabled = true;
+    }
+
+    public void DectivateConfiner() {
+        confiner.enabled = false;
+    }
+}
